b721c30a5fa836edde17f9d4a1830a1015df9f15
Allielkhateb <allielkhateb@gmail.com>
2022-01-28 19:39:55 +0200

    first

---
+++ ProjectMVC/ProjectMVC/MVCProject/MVCProject/ViewModel/Signup.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.ViewModel
+{
+    public class Signup
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
